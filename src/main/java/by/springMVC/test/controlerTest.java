0b50a42c2d8fa4a2a011d89d9c1b74ee3323d466
dudko <oleganeta@mail.ru>
2021-03-04 16:34:32 +0300

    add spring

---
+++ src/main/java/by/springMVC/test/controlerTest.java
@@ -0,0 +1,15 @@
+package by.springMVC.test;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controlerTest {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+
+
+    }
+}
